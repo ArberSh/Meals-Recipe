61e4edfa97a363f59dc5b8f9bc491962c2088d83
ArberSh <arbershaska2@gmail.com>
2023-08-12 23:03:45 +0200

    Trying to fix SearchBar And improved style

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
-async function main() {
-  const input = document.querySelector("#input").value;
-  const response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`
-  );
-  const data = await response.json();
-  const mealWrapper = document.querySelector(".Meal--wrapper");
-  const prove = document.querySelector("#test");
-  console.log(data);
-  let meal = data.meals;
-  if (input === "" || !meal) {
-    mealWrapper.innerHTML = `<h1 class="message">Please Make sure you write correct</h1>`;
-    return;
+// async function main() {
+//   const input = document.querySelector("#input").value;
+//   const response = await fetch(
+//     `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`
+//   );
+//   const data = await response.json();
+//   const mealWrapper = document.querySelector(".Meal--wrapper");
+//   const prove = document.querySelector("#test");
+//   console.log(data);
+//   let meal = data.meals;
+//   if (input === "" || !meal) {
+//     mealWrapper.innerHTML = `<h1 class="message">Please Make sure you write correct</h1>`;
+//     return;
+//   }
+//   // mealWrapper.innerHTML = meal
+//   //   .map((element) => {
+//   //     return `<a class="Meal-Link" href="#"><div id="sup">
+//   //   <img src="${element.strMealThumb}" alt="">
+//   //   <div class="textCenter">
+//   //   <h1>${element.strMeal}</h1>
+//   //   </div>
+//   //   </div>
+//   //   </a>`;
+//   //   })
+//   //   .join("");
+// }
+// main();
+// const prove = document.querySelector("#test");
+// const input = document.querySelector("#input");
+// function main(){
+//   const input1 = input.value
+//   if(input1 === ""){
+//     prove.innerHTML = `<h1>GABIMMM</h1>`
+//     console.log("Lol")
+//     return;
+//   }
+//   else{
+//     console.log("hi")
+//     prove.innerHTML = `<h1>Hi</h1>`
+//   }
+  
+// }
+const inputElement = document.querySelector("#input");
+const messageElement = document.querySelector("#test");
+
+function main() {
+  const input = inputElement.value;
+
+  if (input === "") {
+    messageElement.innerHTML = "<h1>GABIMMM</h1>";
+    console.log("Lol");
+  } else {
+    console.log("hi");
+    messageElement.innerHTML = "<h1>Hi</h1>";
   }
-  mealWrapper.innerHTML = meal
-    .map((element) => {
-      return `<a href="#"><div id="sup">
-    <img src="${element.strMealThumb}" alt="">
-    <div class="textCenter">
-    <h1>${element.strMeal}</h1>
-    </div>
-    </div>
-    </a>`;
-    })
-    .join("");
 }
-main();
